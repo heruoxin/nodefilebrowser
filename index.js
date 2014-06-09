c63681c0d8a072e409d752e638a5fc86d4ee16f1
heruoxin <heruoxin@gmail.com>
2014-06-09 23:29:46 +0800

    fix bug: href start by "."

--- index.js
+++ index.js
@@ -15,26 +15,26 @@ if (process.argv.length == 2){
 }
 
 function Server (req, res){
-  var the_path = decodeURI(url.parse(req.url,true).pathname);
-  console.log("." + the_path);
-  fs.exists("." + the_path, function (exists) { //Is the path exists?
+  var the_path = "." + decodeURI(url.parse(req.url,true).pathname);
+  console.log(the_path);
+  fs.exists(the_path, function (exists) { //Is the path exists?
     if (!exists){
       // 404
       res.writeHead(404, {"Content-Type": "text/plain"});
       res.end("404 Not Found");
       return;
     }
-      if (fs.statSync("." + the_path).isDirectory()) { //Is the path refer to a file or direction?
+      if (fs.statSync(the_path).isDirectory()) { //Is the path refer to a file or direction?
         //for direction
         res.writeHead(200, {
           "Content-Type": "text/html; charset=UTF-8"
         });
-        res.write('<h3>' + "." + the_path + '</h3>');
-        exec('ls ' + "." + the_path, function (err, stout, sterr){
+        res.write('<h3>' + the_path + '</h3>');
+        exec('ls ' + the_path, function (err, stout, sterr){
           var file_list = stout.split('\n');
           for (var i in file_list){
             if (file_list[i] !== "") {
-              res.write('<a href=.' + encodeURI((the_path + '/').replace("//","/") + file_list[i]) + '>' + file_list[i] + '</a>' + '\n');
+              res.write('<a href=' + encodeURI((the_path + '/').replace("//","/").replace(/^\./,"") + file_list[i]) + '>' + file_list[i] + '</a>' + '\n');
               res.write('<br>');
             }
           }
@@ -43,7 +43,6 @@ function Server (req, res){
       } else {
         //for file
         res.writeHead(200, {"Content-Type": "application/octet-stream"});//this should not be text!
-      console.log('5');
         fs.createReadStream("." + the_path).pipe(res);
       }
 
